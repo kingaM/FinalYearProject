54bb40b918400889881f98d307c9fb1602d880f2
Kinga M <kinga_2009@yahoo.com>
2015-03-07 23:24:09 +0000

    Updating setup script

--- setUpEnv.sh
+++ setUpEnv.sh
@@ -1,6 +1,7 @@
 #On VM
-sudo apt-get install make flex bison g++ gcc tmux libboost-all-dev python-pip
+sudo apt-get install make flex bison g++ gcc tmux libboost-all-dev python-pip python-matplotlib r-base-core 
 sudo easy_install pyevolve
+pip install jinja2 networkx
 tar xvfz omnetpp-4.6-src.tgz
 cd omnetpp-4.6
  . setenv
@@ -10,7 +11,10 @@ source ~/.bashrc
 NO_TCL=1 ./configure
 make -j4
 
+sudo R CMD INSTALL omnetpp_0.2-1.tar.gz
+sudo Rscript -e 'install.packages(c("plyr", "ggplot2", "reshape", "gridExtra"))'
+
 #On host machine
 scp -rp FinalYearProject/ azureuser@gpwsn.cloudapp.net:.
 scp omnetpp-4.6-src.tgz azureuser@gpwsn.cloudapp.net:.
-
+scp -r Downloads/omnetpp_0.2-1.tar.gz azureuser@gpawsn.cloudapp.net:.
